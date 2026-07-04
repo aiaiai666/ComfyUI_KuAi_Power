600dd5409114567462ea5f16a67e6c3aa56fb0b9
aiaiai666 <ihuangke1666@gmail.com>
2026-07-05 02:21:07 +0800

    update nano banana model options

--- nodes/NanoBanana/nano_banana.py
+++ nodes/NanoBanana/nano_banana.py
@@ -19,8 +19,10 @@ from ..Sora2.kuai_utils import (
 
 
 NANO_BANANA_MODELS = [
-    "gemini-3-pro-image-preview",
+    "gemini-3.1-flash-image",
+    "gemini-3-pro-image",
     "gemini-3.1-flash-image-preview",
+    "gemini-3-pro-image-preview",
     "gemini-2.5-flash-image",
     "gemini-2.5-flash-image-preview",
 ]
